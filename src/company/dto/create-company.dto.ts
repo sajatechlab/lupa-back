344fd2ac587c18d0e7050fb28d1df61c64cba3c0
Farid Sesin <faridsesin@gmail.com>
2025-04-04 15:36:19 -0500

    Enhance CreateCompanyDto with additional fields and validation
    - Add optional string properties for various company details, including additionalAccountID, industryClassificationCode, and contact information.
    - Ensure nit and name properties are required with @IsNotEmpty validation.
    - Improve data validation and structure for better API integration and data handling.

--- src/company/dto/create-company.dto.ts
+++ src/company/dto/create-company.dto.ts
@@ -1,23 +1,87 @@
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
 export class CreateCompanyDto {
+  @IsString()
+  @IsOptional()
   additionalAccountID?: string;
+
+  @IsString()
+  @IsOptional()
   industryClassificationCode?: string;
+
+  @IsString()
+  @IsOptional()
   physicalLocationID?: string;
+
+  @IsString()
+  @IsOptional()
   countrySubentityCode?: string;
+
+  @IsString()
+  @IsOptional()
   line?: string;
+
+  @IsString()
+  @IsOptional()
   registrationName?: string;
+
+  @IsString()
+  @IsOptional()
   taxLevelCode?: string;
+
+  @IsString()
+  @IsOptional()
   taxSchemeID?: string;
+
+  @IsString()
+  @IsOptional()
   taxSchemeName?: string;
+
+  @IsString()
+  @IsOptional()
   companyID?: string;
+
+  @IsString()
+  @IsOptional()
   companyIDSchemeID?: string;
+
+  @IsString()
+  @IsOptional()
   companyIDSchemeName?: string;
+
+  @IsString()
+  @IsOptional()
   companyIDSchemeAgencyID?: string;
+
+  @IsString()
+  @IsOptional()
   contactName?: string;
+
+  @IsString()
+  @IsOptional()
   contactTelephone?: string;
+
+  @IsString()
+  @IsOptional()
   contactTelefax?: string;
+
+  @IsString()
+  @IsOptional()
   contactElectronicMail?: string;
+
+  @IsString()
+  @IsOptional()
   contactNote?: string;
+
+  @IsString()
+  @IsOptional()
   registrationAddressID?: string;
+
+  @IsString()
+  @IsNotEmpty()
   nit: string;
+  
+  @IsString()
+  @IsNotEmpty()
   name: string;
 }
